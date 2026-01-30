1a07cb1e65a672d980f180388fb1e641ff32722d
anna belenko <66851299+annabelenko@users.noreply.github.com>
2026-01-30 10:14:52 -0500

    Add string functions: capitalize, reverse, countVowels

--- index.js
+++ index.js
@@ -22,3 +22,24 @@ function min(arr) {
   // Hint: Use Math.min(...arr)
   return Math.min(...arr);
 }
+
+// ============ PART 2: STRING FUNCTIONS ============
+function capitalize(str) {
+  // TODO: Return string with first letter capitalized
+  // "hello" -> "Hello"
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function reverse(str) {
+  // TODO: Return the string reversed
+  // "hello" -> "olleh"
+  // Hint: str.split("").reverse().join("")
+  return str.split("").reverse().join("");
+}
+
+function countVowels(str) {
+  // TODO: Return count of vowels (a, e, i, o, u)
+  // "hello" -> 2
+  const vowels = 'aeiouAEIOU';
+  return str.split('').filter(char => vowels.includes(char)).length;
+}
